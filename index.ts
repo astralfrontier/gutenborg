d0006fe4b3159d73fa831f838c8ed823acd7c739
Bill Garrett <garrett@astralfrontier.org>
2025-03-11 15:02:45 -0700

    Use commander to parse CLI options

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello World");
+import { Command } from "commander";
+
+import { version, description } from "./package.json";
+
+const program = new Command();
+
+program.version(version).description(description).parse(process.argv);
+
+const options = program.opts();
